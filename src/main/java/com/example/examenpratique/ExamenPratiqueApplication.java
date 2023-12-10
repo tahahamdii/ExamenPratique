e8e561168ab93058ddde29a2288d1b58acfa7a78
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-12-10 20:06:12 +0100

    first comit

---
+++ src/main/java/com/example/examenpratique/ExamenPratiqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.examenpratique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPratiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenPratiqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examenpratique/ExamenPratiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenpratique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenPratiqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
